02596cd8b790b0ab847f10a412b211c9b6584722
Iker <iker.a@opendeusto.es>
2024-11-08 11:32:14 +0100

    Add sample challenges and training sessions in DatosMuestra; update StravaService to manage challenges and training sessions more effectively.

--- src/main/java/es/deusto/sd/strava/service/StravaService.java
+++ src/main/java/es/deusto/sd/strava/service/StravaService.java
@@ -16,6 +16,7 @@ public class StravaService {
 
     private List<Usuario> usuarios = new ArrayList<>();
     private List<Reto> retos = new ArrayList<>();
+    private List<Entrenamiento> entrenamientos = new ArrayList<>();
     
 
     public boolean registrarUsuario(Usuario u) {
@@ -33,16 +34,18 @@ public class StravaService {
 
     // Get all entrenamientos
     public List<Entrenamiento> consultarEntrenamientos() {
-        return entrenamientoRepository.values().stream().toList();
+        //return entrenamientoRepository.values().stream().toList();
+        return entrenamientos;
     }
 
     // Get all retos
     public List<Reto> consultarRetos() {
-        return retoRepository.values().stream().toList();
+        //return retoRepository.values().stream().toList();
+        return retos;
     }
 
     //Create a new entrenamiento
-    public String crearEntrenamiento(Entrenamiento e) {
+    /* public String crearEntrenamiento(Entrenamiento e) {
         Entrenamiento entrenamiento = new Entrenamiento();
         entrenamiento.setDistancia(e.getDistancia());
         entrenamiento.setDuracion(e.getDuracion());
@@ -51,9 +54,15 @@ public class StravaService {
         entrenamiento.setDeporte(e.getDeporte());
         entrenamiento.setTitulo(e.getTitulo());
         return "Entrenamiento creado exitosamente";
+    }*/
+
+    public String crearEntrenamiento(Entrenamiento entrenamiento) {
+        //entrenamientoRepository.put(entrenamiento.getId(), entrenamiento);
+        entrenamientos.add(entrenamiento);
+        return "Entrenamiento registrado con éxito";
     }
 
-    public String crearReto(Reto r) {
+    /*public String crearReto(Reto r) {
         Reto reto = new Reto();
         reto.setObjetivoDistancia(r.getObjetivoDistancia());
         reto.setObjetivoTiempo(r.getObjetivoTiempo());
@@ -63,5 +72,13 @@ public class StravaService {
         reto.setNombre(r.getNombre());
         retos.add(reto);
         return "Reto creado exitosamente";
+    }*/
+    public String crearReto(Reto reto) {
+        //retoRepository.put(reto.getId(), reto);
+        retos.add(reto);
+        return "Reto registrado con éxito";
     }
+
+
+    
 }
