2c5bcf8846f58e4f36b9d40424708f900c4d1cce
Iker <iker.a@opendeusto.es>
2024-11-17 21:14:55 +0100

    Remove unused Logger imports in StravaController for cleaner code

--- src/main/java/es/deusto/sd/strava/facade/StravaController.java
+++ src/main/java/es/deusto/sd/strava/facade/StravaController.java
@@ -31,8 +31,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api")
